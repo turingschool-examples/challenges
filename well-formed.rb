1fc3966dbd424d0c1cab023f8f1dae2923dc6f1b
Noah Berman <noah@noahberman.org>
2017-02-03 10:47:51 -0700

    working solution to initial problem

--- well-formed.rb
+++ well-formed.rb
@@ -7,10 +7,23 @@ class StringValidator
     if trimmed_string.size.odd?
       return false
     elsif trimmed_string.empty?
-      return false
-    else
       return true
+    else
+      return even_string_validator(string)
     end
+  end
+  
+  def even_string_validator(string)
+    symbols_to_check = { '{' => '}', 
+                         '[' => ']',
+                         '(' => ')'}
+    check_array = []
+    string.split('').each do |letter|
+      check_array << letter if symbols_to_check.key?(letter)
+      return false if symbols_to_check.key(letter) && 
+                      symbols_to_check.key(letter) != check_array.pop
+     end
+     check_array.empty?
   end
   
 end
