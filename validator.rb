0aacc93d076012f8794c86d4ba7a5454816bbeb6
Chase Dunagan <cdunagan@utexas.edu>
2017-02-03 11:53:45 -0700

    added beginning of Jeffs code

--- validator.rb
+++ validator.rb
@@ -1,16 +1,22 @@
 class Validator
-  def validate(string)
-    split_string = string.split("")
-    split_string.each do |i|
-      first = split_string[i]
-      split_string.shift
-      split_string.each do |i|
-        if split_string[i] == first
-          array = split_string.shift((i + 1))
-          if i.length = 1
-            break;
-            true
-          else
-            array.each do |i|
+
+  def pairings
+    {
+      "(" => ")",
+      "[" => "]",
+      "{" => "}",
+    }
   end
+
+  def matched_pair?(t1, t2)
+    pairings[t1] == [t2]
+  end
+
+
+  def validate(target)
+    true if target.empty?
+    tokens = target.chars
+    matched_pair?(tokens[0], tokens[1])
+  end
+
 end
