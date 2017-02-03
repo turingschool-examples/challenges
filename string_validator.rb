24f51f6cd08905f0fccb4bdb849d8f424be83258
Jeff Casimir <jeff@casimircreative.com>
2017-02-03 11:14:26 -0700

    Validates empty string and single parens

--- string_validator.rb
+++ string_validator.rb
@@ -1,3 +1,15 @@
 class StringValidator
-
+  def validate(target)
+    # consider just "()" and ")("
+    if target.length > 0
+      tokens = target.chars
+      if tokens[0] == "(" && tokens[1] == ")"
+        true
+      else
+        false
+      end
+    else
+      true
+    end
+  end
 end
--- string_validator_test.rb
+++ string_validator_test.rb
@@ -5,4 +5,20 @@ class StringValidatorTest < Minitest::Test
   def test_it_exists
     assert StringValidator
   end
+
+  def test_an_empty_string_is_valid
+    sv = StringValidator.new
+    assert sv.validate("")
+  end
+
+  def test_single_parens_are_valid
+    sv = StringValidator.new
+    assert sv.validate("()")
+  end
+
+  def test_single_inverted_parens_are_not_valid
+    sv = StringValidator.new
+    refute sv.validate(")(")
+  end
+
 end
