112b7cf472d8e046b8b5b91ef4bed118cc2b71d3
Matt D <MDes41@users.noreply.github.com>
2017-02-03 10:45:36 -0700

    Stepping through with pry

--- string_validator.rb
+++ string_validator.rb
@@ -1,6 +1,22 @@
+require 'pry'
 class StringValidator
 	def validate(string)
-		
+		arr = string.split('')
+		length = arr.length
+
+		(length).times do 
+			in_question = arr.shift
+			case in_question
+			when '('
+				pair = ')'
+			when '['
+				pair = ']'
+			when '{'
+				pair = '}'
+			end
+			return false if !arr.include?(pair)
+		end
+		return true
 	end
 end
 
