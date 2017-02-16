dab1e1d68abed94fc95fccbfec77e0ad80178daf
Lee Saville <l.w.saville@gmail.com>
2017-02-16 13:31:32 -0700

    Add test for count for letter case

--- pattern_generator.rb
+++ pattern_generator.rb
@@ -1,7 +1,11 @@
 class PatternGenerator
 
   def self.generate_serial(pattern)
-  { patterns: [0,1,2,3,4,5,6,7,8,9], count: 10 }
+    if pattern == 'N'
+      { patterns: [0,1,2,3,4,5,6,7,8,9], count: 10 }
+    elsif pattern == 'X'
+      { patterns: ('A'..'Z').to_a, count: 26}
+    end
   end
 
 end
--- pattern_generator_test.rb
+++ pattern_generator_test.rb
@@ -13,7 +13,7 @@ class PatternGeneratorTest < Minitest::Test
     assert_equal result[:patterns], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  def test_it_gives_a_count_of_the_patterns_for_a_single_character_pattern
+  def test_it_gives_a_count_of_the_patterns_for_a_single_character_number_pattern
     result = @pg.generate_serial('N')
 
     assert_equal result[:count], 10
@@ -23,5 +23,11 @@ class PatternGeneratorTest < Minitest::Test
     result = @pg.generate_serial('X')
 
     assert_equal result[:patterns], ('A'..'Z').to_a
+  end
+
+  def test_it_gives_a_count_of_the_patterns_for_a_single_character_letter_pattern
+    result = @pg.generate_serial('X')
+
+    assert_equal result[:count], 26
   end
 end
