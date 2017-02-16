c1916caeedfd3fb5247a1c486672b228ab4f0bd8
Lee Saville <l.w.saville@gmail.com>
2017-02-16 13:47:50 -0700

    Add some wild spiking

--- pattern_generator.rb
+++ pattern_generator.rb
@@ -2,10 +2,27 @@ class PatternGenerator
 
   def self.generate_serial(pattern)
     if pattern == 'N'
-      { patterns: [0,1,2,3,4,5,6,7,8,9], count: 10 }
+      { patterns: ['0','1','2','3','4','5','6','7','8','9'], count: 10 }
     elsif pattern == 'X'
       { patterns: ('A'..'Z').to_a, count: 26}
     end
+  end
+
+  def self.generate_serial(pattern)
+    all_patterns = []
+    pattern.split.each do |letter|
+      if pattern == 'N'
+        #do some crazy building here
+      elsif pattern == 'X'
+        #do some more crazy building here
+      end
+    end
+  end
+
+  def letter_library
+  end
+
+  def number_library
   end
 
 end
