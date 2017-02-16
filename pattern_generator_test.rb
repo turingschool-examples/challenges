caa3e863ee06a6675e16de73cc6d4326c6ad8b8a
Lee Saville <l.w.saville@gmail.com>
2017-02-16 13:25:04 -0700

    Change naming so that it makes more sense

--- pattern_generator_test.rb
+++ pattern_generator_test.rb
@@ -6,13 +6,20 @@ class PatternGeneratorTest < Minitest::Test
   def setup
     @pg = PatternGenerator
   end
-  def test_it_works_for_a_single_character_serial_number
+
+  def test_it_works_for_a_single_number_character_pattern
     result = @pg.generate_serial('N')
 
     assert_equal result[:patterns], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  def test_it_works_for_a_single_character_letter
+  def test_it_gives_a_count_of_the_patterns_for_a_single_character_pattern
+    result = @pg.generate_serial('N')
+
+    assert_equal result[:count], 10
+  end
+
+  def test_it_works_for_a_single_character_letter_pattern
     result = @pg.generate_serial('X')
 
     assert_equal result[:patterns], ('A'..'Z').to_a
