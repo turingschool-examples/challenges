826fe223f0bf392245d8b541a725f22e1dca4d27
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-16 13:05:59 -0700

    Test it is 5 char long passing

---
+++ pattern_generator/lib/pattern_generator.rb
@@ -0,0 +1,11 @@
+class PatternGenerator
+
+  def initialize
+    @pattern = ""
+  end
+
+  def new_pattern
+    "XXNXN"
+  end
+
+end
--- pattern_generator/test/pattern_generator_test.rb
+++ pattern_generator/test/pattern_generator_test.rb
@@ -5,6 +5,11 @@ require './lib/pattern_generator'
 
 class PatternGeneratorTest < Minitest::Test
 
+  def test_it_can_generate_a_pattern
+    pattern_generator = PatternGenerator.new
+    pattern = pattern_generator.new_pattern
+    assert_equal 5, pattern.length
+  end
 
 
 end
