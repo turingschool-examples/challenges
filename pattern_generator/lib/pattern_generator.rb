aa9f0947f7622c041e717e2c69e9619224d24efb
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-16 14:41:06 -0700

    Attempt at all serials, stuck in infinite loop

--- pattern_generator/lib/pattern_generator.rb
+++ pattern_generator/lib/pattern_generator.rb
@@ -6,35 +6,54 @@ class PatternGenerator
                 "H", "J", "K", "L", "M", "N", "P",
                 "Q", "R", "S", "T", "U", "V", "W",
                 "X", "Y", "Z"]
+    @serials = {}
+    @total = (10**pattern.count("N"))*(26**pattern.count("X"))
+    @count = 0
   end
 
-  def find_serials
+
+  def find_serial
     serial = @pattern.reduce("") do |result, char_type|
       result += random_capital_letter if char_type == "X"
-      result += random_number.to_s if char_type == "N"
+      result += random_number.to_s    if char_type == "N"
       result
     end
   end
 
-  def new_serial
-    serial = ""
-    serial += random_capital_letter
-    serial += random_capital_letter
-    serial += random_number.to_s
-    serial += random_capital_letter
-    serial += random_number.to_s
-  end
-
   def random_capital_letter
-    @letters[random_number]
+    @letters[random_selector]
   end
 
   def random_number
     Random.rand(2..9)
   end
 
-  def all_patterns
-
+  def random_selector
+    Random.rand(0..23)
   end
+
+  def all_serials
+    until @count != @total
+      new_serial = find_serial
+      if !@serials.has_key?(new_serial)
+        @serials[new_serial] = 1
+        @count += 1
+      end
+    end
+    @serials
+  end
+
+  # def all_serials
+  #   # first char  all positions 0 - 23 rest 2
+  #   # second char all positions 1 - 23 rest A/2
+  #   # third char all numbers 3-9 rest A/2
+  #   # fourth char all positions 1 - 23 rest A/2
+  #   # fifth char all numbers 3-9 rest A/2
+  #   all_for_first_char
+  # end
+  #
+  # def all_for_first_char
+  #
+  # end
 
 end
