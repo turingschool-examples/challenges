b19621ee22ac9648f0aa93e4b31435f0a191003a
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-16 13:48:58 -0700

    Update to work for any pattern combo

--- pattern_generator/lib/pattern_generator.rb
+++ pattern_generator/lib/pattern_generator.rb
@@ -1,27 +1,40 @@
 class PatternGenerator
 
-  def initialize
+  def initialize(pattern)
+    @pattern = pattern.chars
     @letters = ["A", "B", "C", "D", "E", "F", "G",
                 "H", "J", "K", "L", "M", "N", "P",
                 "Q", "R", "S", "T", "U", "V", "W",
                 "X", "Y", "Z"]
-    @pattern = ""
   end
 
-  def new_pattern
-    @pattern += random_capital_letter
-    @pattern += random_capital_letter
-    @pattern += random_selector.to_s
-    @pattern += random_capital_letter
-    @pattern += random_selector.to_s
+  def find_serials
+    serial = @pattern.reduce("") do |result, char_type|
+      result += random_capital_letter if char_type == "X"
+      result += random_number.to_s if char_type == "N"
+      result
+    end
+  end
+
+  def new_serial
+    serial = ""
+    serial += random_capital_letter
+    serial += random_capital_letter
+    serial += random_number.to_s
+    serial += random_capital_letter
+    serial += random_number.to_s
   end
 
   def random_capital_letter
-    @letters[random_selector]
+    @letters[random_number]
   end
 
-  def random_selector
+  def random_number
     Random.rand(2..9)
+  end
+
+  def all_patterns
+
   end
 
 end
--- pattern_generator/test/pattern_generator_test.rb
+++ pattern_generator/test/pattern_generator_test.rb
@@ -19,57 +19,57 @@ class PatternGeneratorTest < Minitest::Test
   end
 
   def test_it_can_generate_a_pattern
-    pattern_generator = PatternGenerator.new
-    pattern = pattern_generator.new_pattern
+    pattern_generator = PatternGenerator.new("XXNXN")
+    pattern = pattern_generator.find_serials
 
     assert_equal 5, pattern.length
   end
 
   def test_the_first_char_is_a_captial_letter
     set_up
-    pattern_generator = PatternGenerator.new
-    pattern = pattern_generator.new_pattern
+    pattern_generator = PatternGenerator.new("XXNXN")
+    pattern = pattern_generator.find_serials
 
     assert @letters[pattern[0]]
   end
 
   def test_the_second_char_is_a_capital_letter
     set_up
-    pattern_generator = PatternGenerator.new
-    pattern = pattern_generator.new_pattern
+    pattern_generator = PatternGenerator.new("XXNXN")
+    pattern = pattern_generator.find_serials
 
     assert @letters[pattern[1]]
   end
 
   def test_the_third_char_is_a_number
     set_up
-    pattern_generator = PatternGenerator.new
-    pattern = pattern_generator.new_pattern
+    pattern_generator = PatternGenerator.new("XXNXN")
+    pattern = pattern_generator.find_serials
 
     assert @numbers[pattern[2]]
   end
 
   def test_the_fourth_char_is_a_capital_letter
     set_up
-    pattern_generator = PatternGenerator.new
-    pattern = pattern_generator.new_pattern
+    pattern_generator = PatternGenerator.new("XXNXN")
+    pattern = pattern_generator.find_serials
 
     assert @letters[pattern[3]]
   end
 
   def test_the_fifth_char_is_a_number
     set_up
-    pattern_generator = PatternGenerator.new
-    pattern = pattern_generator.new_pattern
+    pattern_generator = PatternGenerator.new("XXNXN")
+    pattern = pattern_generator.find_serials
 
     assert @numbers[pattern[4]]
   end
 
   def test_two_patterns_are_not_the_same
     set_up
-    pattern_generator = PatternGenerator.new
-    pattern_1 = pattern_generator.new_pattern
-    pattern_2 = pattern_generator.new_pattern
+    pattern_generator = PatternGenerator.new("XXNXN")
+    pattern_1 = pattern_generator.find_serials
+    pattern_2 = pattern_generator.find_serials
 
     refute pattern_1 == pattern_2
   end
