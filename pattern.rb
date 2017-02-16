95ed2687358c0a23abfdf827c5faf3358caf35a4
Noah Berman <noah@noahberman.org>
2017-02-16 13:14:26 -0700

    random generator is working

--- pattern.rb
+++ pattern.rb
@@ -1,4 +1,22 @@
+require 'pry'
+
 class PatternMatcher
+  attr_reader :serial
+  
+  def serial_generator(pattern)
+    @serial = []
+    pattern.upcase.chars.each do |character|
+      if character == "X"
+        @serial.push(random_letter)
+      elsif character == "N"
+        @serial.push(random_number)
+      else
+        puts "Error."
+      end
+      @serial
+    end
+    @serial
+  end
   
   def random_number
     numbers = (0..9).to_a
--- pattern_test.rb
+++ pattern_test.rb
@@ -21,4 +21,14 @@ class PatternMatcherTest < Minitest::Test
     assert_equal String, letter.class
   end
 
+  def test_that_the_matcher_outputs_the_right_thing_for_X
+    serial = @p.serial_generator("X")
+    refute_equal Fixnum, serial[0].class
+  end
+
+  def test_that_the_matcher_outputs_the_right_thing_for_N
+    serial = @p.serial_generator("N")
+    assert_equal Fixnum, serial[0].class
+  end
+  
 end
