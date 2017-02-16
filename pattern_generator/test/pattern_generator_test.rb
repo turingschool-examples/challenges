beb4e56571699540bd58e99ca0668bbde4bdb6ed
Ali Schlereth <aschlere@sbcglobal.net>
2017-02-16 13:16:40 -0700

    Checks char is a capital letter

--- pattern_generator/test/pattern_generator_test.rb
+++ pattern_generator/test/pattern_generator_test.rb
@@ -5,10 +5,27 @@ require './lib/pattern_generator'
 
 class PatternGeneratorTest < Minitest::Test
 
+  def set_up
+    @letters = {"A"=> true, "B"=> true, "C"=> true, "D"=> true,
+              "E"=> true, "F"=> true, "G"=> true, "H"=> true,
+              "I"=> true, "J"=> true, "K"=> true, "L"=> true,
+              "M"=> true, "N"=> true, "O"=> true, "P"=> true,
+              "Q"=> true, "R"=> true, "S"=> true, "T"=> true,
+              "U"=> true, "V"=> true, "W"=> true, "X" => true,
+              "Y"=> true, "Z"=> true}
+  end
+
   def test_it_can_generate_a_pattern
     pattern_generator = PatternGenerator.new
     pattern = pattern_generator.new_pattern
     assert_equal 5, pattern.length
+  end
+
+  def test_the_first_char_is_a_letter
+    set_up
+    pattern_generator = PatternGenerator.new
+    pattern = pattern_generator.new_pattern
+    assert @letters[pattern[0]]
   end
 
 
