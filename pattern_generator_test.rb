6d123745a1ecdab57a467d55140a0bdd81d6fc64
Lee Saville <l.w.saville@gmail.com>
2017-02-16 13:48:09 -0700

    Change expectation to strings rather than numbers

--- pattern_generator_test.rb
+++ pattern_generator_test.rb
@@ -10,7 +10,7 @@ class PatternGeneratorTest < Minitest::Test
   def test_it_works_for_a_single_number_character_pattern
     result = @pg.generate_serial('N')
 
-    assert_equal result[:patterns], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert_equal result[:patterns], ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   end
 
   def test_it_gives_a_count_of_the_patterns_for_a_single_character_number_pattern
