f0fb0d33f3abf9672969d4de4eedf1d70aac1220
Erin Pintozzi <erin.pintozzi@gmail.com>
2017-02-16 14:14:15 -0700

    begin to write pattern counter

--- pattern_generator/lib/generator.rb
+++ pattern_generator/lib/generator.rb
@@ -1,21 +1,41 @@
 class Generator
+  attr_accessor :pattern_count
 
+  LETTERS = Array('A'..'Z')
+  NUMBERS = Array(0..9)
+
+  def initialize
+    @pattern_count = 1
+    @serial_number = ''
+  end
 
   def random_letters(pattern)
-    letters = Array('A'..'Z')
-    numbers = Array(0..9)
-    serial_number = []
 
     pattern.each_char do |char|
       if char == "X"
-        letter = letters.sample
-        serial_number << letter
+        letter = LETTERS.sample
+        pattern_counter(26)
+        @serial_number << letter
       elsif char == "N"
-        number = numbers.sample
-        serial_number << number
+        number = NUMBERS.sample
+        pattern_counter(10)
+        @serial_number << number
       end
     end
-    serial_number.join
+    @serial_number
+  end
+
+  def pattern_counter(multiplier)
+    @pattern_count = @pattern_count*multiplier
+  end
+
+  def all_combinations(pattern)
+    combos = LETTERS.repeated_permutation(pattern.length).to_a
+  end
+
+  def combination_count(pattern)
+    random_letters(pattern)
+    @pattern_count
   end
 
 end
--- pattern_generator/test/generator_test.rb
+++ pattern_generator/test/generator_test.rb
@@ -7,11 +7,13 @@ require 'pry'
 class GeneratorTest < Minitest::Test
 
   def test_it_exists
+    skip
     g = Generator.new
     assert g
   end
 
   def test_it_generates_a_single_letter
+    skip
     g = Generator.new
     code = g.random_letters("X")
 
@@ -20,6 +22,7 @@ class GeneratorTest < Minitest::Test
   end
 
   def test_it_generates_five_letters
+    skip
     g = Generator.new
     code = g.random_letters("XXXXX")
 
@@ -28,6 +31,7 @@ class GeneratorTest < Minitest::Test
   end
 
   def test_it_generates_a_letter_and_number
+    skip
     g = Generator.new
     code = g.random_letters("XN")
 
@@ -36,11 +40,79 @@ class GeneratorTest < Minitest::Test
   end
 
   def test_it_generates_a_letter_number_combo
+    skip
     g = Generator.new
     code = g.random_letters("XNXXN")
 
     assert_equal 5, code.length
     assert_equal String, code.class
+  end
+
+  def test_it_generates_all_combos_for_1_letter
+    skip
+    g = Generator.new
+    pattern = "X"
+    combos = g.all_combinations(pattern)
+
+    assert_equal 26, combos.count
+    assert_equal Array, combos.class
+  end
+
+  def test_it_generates_all_combos_for_2_letters
+skip
+    g = Generator.new
+    pattern = "XX"
+    combos = g.all_combinations(pattern)
+
+    assert_equal 676, combos.count
+    assert_equal Array, combos.class
+  end
+
+  def test_it_generates_all_combos_for_1_letter_1_number
+
+    g = Generator.new
+    pattern = "XN"
+    combos = g.all_combinations(pattern)
+
+    assert_equal 260, combos.count
+    assert_equal Array, combos.class
+  end
+
+  def test_it_counts_all_combos_for_2_letters
+skip
+    g = Generator.new
+    pattern = "XX"
+    combos = g.combination_count(pattern)
+
+    assert_equal 676, combos
+  end
+
+  def test_it_counts_all_combos_for_1_letter_1_number
+    skip
+    g = Generator.new
+    pattern = "XN"
+    combos = g.combination_count(pattern)
+
+    assert_equal 260, combos
+  end
+
+  def test_it_generates_all_combos_for_5_letters
+    skip
+    g = Generator.new
+    pattern = "XXXXX"
+    combos = g.all_combinations(pattern)
+
+    assert_equal 11881376, combos.count
+    assert_equal Array, combos.class
+  end
+
+  def test_it_count_total_combos_for_5_letters
+    skip
+    g = Generator.new
+    pattern = "XXXXX"
+    combos = g.combination_count(pattern)
+
+    assert_equal 11881376, combos
   end
 
 end
